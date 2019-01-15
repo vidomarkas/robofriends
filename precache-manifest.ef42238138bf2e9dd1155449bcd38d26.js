0e6bcd4cdf44e9236991430b8b9ee017fb531062
Viktoras Domarkas <vidomarkas@gmail.com>
2019-01-15 12:22:53 +0000

    Updates

---
+++ precache-manifest.ef42238138bf2e9dd1155449bcd38d26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd2fa32782b23b35be00",
+    "url": "/static/js/main.cd2fa327.chunk.js"
+  },
+  {
+    "revision": "1cca5715b6ea4f220239",
+    "url": "/static/js/1.1cca5715.chunk.js"
+  },
+  {
+    "revision": "cd2fa32782b23b35be00",
+    "url": "/static/css/main.90e9b74f.chunk.css"
+  },
+  {
+    "revision": "1cca5715b6ea4f220239",
+    "url": "/static/css/1.658fa91b.chunk.css"
+  },
+  {
+    "revision": "0db897aed22a8f149f4551e042bee9a3",
+    "url": "/index.html"
+  }
+];
